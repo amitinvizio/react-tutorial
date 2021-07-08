cfac3abbced4a7bab2e3d930a636e94c53935f62
amitinvizio <Amit@inviziosolutions.com>
2021-07-08 15:50:22 +0530

    CSS Style with date and time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
-function Header(){
+function Header() {
+  let color = ''
+
+  // var currDate = new Date(2021, 7, 5, 11)
+  var currDate = new Date()
+  currDate = currDate.getHours()
+  var greeting = ''
+  if (currDate > 1 && currDate <= 11) {
+    color = "Red"
+    greeting = 'Morning'
+  } else if (currDate >= 12 && currDate <= 16) {
+    color = "Orange"
+    greeting = 'Afternoon'
+  } else if (currDate >= 17 && currDate <= 20) {
+    color = "Aqua"
+    greeting = 'Evening'
+  } else {
+    color = "Black"
+    greeting = 'Night'
+  }
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World, Good <span style={{color}}> {greeting} </span></h1>
     </>
   )
 }
