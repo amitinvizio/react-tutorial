266bbce85312e6f849bdd83c7570d002113e29e4
amitinvizio <Amit@inviziosolutions.com>
2021-07-10 13:42:34 +0530

    Add Routing for home users and about pages using react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/Header';
+import Navigation from './components/Navigation';
 
 ReactDOM.render(
-  <Header/>,
+  <>
+    <Navigation />
+    <Header />
+  </>,
   document.getElementById('root')
 );
 
